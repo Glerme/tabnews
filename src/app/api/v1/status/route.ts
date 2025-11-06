fb80428504c4051262a5214d8ed3325a621092e5
Glerme <guiggff@gmail.com>
2025-11-06 17:46:12 -0300

    finish /api/v1/status

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,3 +1,35 @@
-export async function GET() {
-  return Response.json({ message: "Hello World" }, { status: 200 });
+import type { NextRequest } from "next/server";
+import { query } from "@/infra/database";
+
+export async function GET(request: NextRequest) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionQuery = await query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionQuery.rows[0].server_version;
+
+  const databaseMaxConnectionsQuery = await query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsQuery = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsQuery.rows[0].count;
+
+  console.log("databaseOpenedConnectionsValue", databaseOpenedConnectionsValue);
+
+  return Response.json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: Number(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,10 +1,17 @@
-import { query } from "@/infra/database";
-
 test("GET to /api/v1/status should return 200", async () => {
-  const result = await query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-
-  const response = await fetch("http://localhost:3000/api/v1/status");
-
+  const response = await fetch(
+    "http://localhost:3000/api/v1/status?databaseName="
+  );
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
